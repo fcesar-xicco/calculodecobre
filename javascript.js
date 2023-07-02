60ba31b1650c0a5925384da6ab6dcb88a001699f
Francisco Cesar <117320144+fcesar-xicco@users.noreply.github.com>
2023-07-02 14:32:17 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,34 +1,37 @@
 function verificar(){
     //window.alert("deu certo")
-    var qtdmetros = window.document.getElementById("qtdmetro")
-    var resp = window.document.getElementById("resp")
+    let qtdmetros = window.document.getElementById("qtdmetro")
+    let resp = window.document.getElementById("resp")
     //resp.innerText ='Oi'
 
-    var valorkg = 89.90
-    var valordescont = 84.90
-    var valorkgp = 84.90
-    var valordescontp = 79.90
+    let valorkg = 89.90
+    let valordescont = 84.90
+    let valorkgp = 84.90
+    let valordescontp = 79.90
+
+    let bitselect = ''
+        let pesoselec = 0
   
    if(qtdmetros.value.length == 0){
      window.alert("[ERROR] digite todas as informações corretamente")
    }
    else {
-     var respbit = window.document.getElementsByClassName("cbit")
+     let respbit = window.document.getElementsByClassName("cbit")
      if(respbit[0].checked){
-        var bitselect = '1/4'
-        var pesoselec = 0.130
+        bitselect = '1/4'
+        pesoselec = 0.130
       }else if(respbit[1].checked){
-       var bitselect = '3/8'
-        var pesoselec = 0.200
+        bitselect = '3/8'
+        pesoselec = 0.200
      }else if(respbit[2].checked){
-        var bitselect = '1/2'
-        var pesoselec = 0.270
+        bitselect = '1/2'
+        pesoselec = 0.270
      }else if(respbit[3].checked){
-        var bitselect = '5/8'
-        var pesoselec = 0.350
+        bitselect = '5/8'
+        pesoselec = 0.350
      }else if(respbit[4].checked) {
-        var bitselect = '3/4'
-        var pesoselec = 0.410
+        bitselect = '3/4'
+        pesoselec = 0.410
      }
    
 
@@ -37,8 +40,8 @@ function verificar(){
       var valordescont = 84.90
       
    }else{
-      var valorkg = 84.90
-      var valordescont = 79.90
+      valorkg = 84.90
+      valordescont = 79.90
    }
         
       var qtdmetros = window.document.getElementById("qtdmetro")
